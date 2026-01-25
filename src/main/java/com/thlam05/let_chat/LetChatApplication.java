1798cba902cc0969a64a435f8c10058598489b08
hlam3004 <hlam.30042005@gmail.com>
2026-01-25 22:53:52 +0700

    add project

---
+++ src/main/java/com/thlam05/let_chat/LetChatApplication.java
@@ -0,0 +1,13 @@
+package com.thlam05.let_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thlam05/let_chat/LetChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thlam05.let_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
