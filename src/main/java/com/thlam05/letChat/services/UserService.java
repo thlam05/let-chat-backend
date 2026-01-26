4b834e7c456e9b0bcf96a58fd438af522c08cf3e
hlam3004 <hlam.30042005@gmail.com>
2026-01-26 23:35:26 +0700

    add  handler exception

---
+++ src/main/java/com/thlam05/letChat/services/UserService.java
@@ -0,0 +1,13 @@
+package com.thlam05.letChat.services;
+
+import org.springframework.stereotype.Service;
+
+import com.thlam05.letChat.repositories.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private UserRepository userRepository;
+}
