089c303450b26d35786aaf923408d2b62ffb727b
hlam3004 <hlam.30042005@gmail.com>
2026-01-27 22:58:00 +0700

    add class SecurityConfig

---
+++ src/main/java/com/thlam05/letChat/configurations/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.thlam05.letChat.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    private final String[] PUBLIC_ENDPOINTS = {
+            "/users"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request -> request
+                .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
+                .requestMatchers(HttpMethod.GET, PUBLIC_ENDPOINTS).permitAll()
+                .anyRequest().authenticated());
+
+        // httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer ->
+        // jwtConfigurer
+        // .decoder(customJwtDecoder)
+        // .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+        // .authenticationEntryPoint(new JwtAuthenticationEntryPoint()));
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+}
--- src/main/java/com/thlam05/letChat/services/UserService.java
+++ src/main/java/com/thlam05/letChat/services/UserService.java
@@ -2,12 +2,12 @@ package com.thlam05.letChat.services;
 
 import org.springframework.stereotype.Service;
 
-import com.thlam05.letChat.repositories.UserRepository;
+// import com.thlam05.letChat.repositories.UserRepository;
 
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class UserService {
-    private UserRepository userRepository;
+    // private UserRepository userRepository;
 }
