a9e6ec6d023b4fb9e9f8a549f19d0c728f99d65b
hlam3004 <hlam.30042005@gmail.com>
2026-02-03 21:19:42 +0700

    rename package to letchat

--- src/main/java/com/thlam05/letChat/services/AuthService.java
+++ src/main/java/com/thlam05/letChat/services/AuthService.java
@@ -3,7 +3,6 @@ package com.thlam05.letchat.services;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
